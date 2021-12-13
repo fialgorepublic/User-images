b5f3fa029e666d5d14092a96c0a796a55f87a6d2
Hasan Ali <hasan@algorepublic.com>
2021-12-13 20:42:59 +0500

    minor changes for API

--- app/controllers/api/v1/user_images_controller.rb
+++ app/controllers/api/v1/user_images_controller.rb
@@ -1,65 +1,69 @@
-class Api::V1::UserImagesController < Api::V1::BaseController
-  before_action :set_user_image, only: %i[ show send_image claim_image]
+class UserImagesController < ApplicationController
+  before_action :set_user_image, only: %i[ show edit update destroy ]
 
+  # GET /user_images or /user_images.json
   def index
-    @user_images = current_user.user_images.all
-    render json: {success: true, message: "All Images", data: @user_images.collect {|image| user_image(image)}}
+    @user_images = UserImage.all
   end
 
+  # GET /user_images/1 or /user_images/1.json
   def show
-    render json: {success: true, message: "Single Image", data: @user_image.attributes.merge(image: @user_image.pic.attached? ? url_for(@user_image.pic) : '')}
   end
 
+  # GET /user_images/new
+  def new
+    @user_image = UserImage.new
+  end
+
+  # GET /user_images/1/edit
+  def edit
+  end
+
+  # POST /user_images or /user_images.json
   def create
-    @user_image = current_user.user_images.new(user_image_params)
-    @user_image.uuid = SecureRandom.uuid
-      if @user_image.save
-        render json: {success: true, message: "Image created successfully", data: @user_image.attributes.merge(image: @user_image.pic.attached? ?  url_for(@user_image.pic) : '')}
-      end
-  end
+    @user_image = UserImage.new(user_image_params)
 
-  def send_image
-    if @user_image.present? && params[:email].present?
-      client = Customerio::APIClient.new('804b58df7d21727da911cd607e7d7c86', region: Customerio::Regions::US)
-      request = Customerio::SendEmailRequest.new(
-        to: params[:email],
-        from: 'team@womboapp.com',
-        subject: 'Image link',
-        body: "nftmaker://147.182.199.116/api/v1/user_images/#{@user_image.uuid}"
-      )
-      begin
-        response = client.send_email(request)
-        if response.code == '200'
-          render json: 'image sent'
-        end
-      rescue Customerio::InvalidResponse => e
-        puts e.code, e.message
+    respond_to do |format|
+      if @user_image.save
+        format.html { redirect_to @user_image, notice: "User image was successfully created." }
+        format.json { render :show, status: :created, location: @user_image }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user_image.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  def claim_image
-    if @user_image && @user_image.update(user_id: current_user.id)
-      render json: {success: true, message: "Image claimed successfully", data: @user_image.attributes.merge(image: @user_image.pic.attached? ?  url_for(@user_image.pic) : '')}
-    else
-      render json: {success: false, message: "Image not present"}
+  # PATCH/PUT /user_images/1 or /user_images/1.json
+  def update
+    respond_to do |format|
+      if @user_image.update(user_image_params)
+        format.html { redirect_to @user_image, notice: "User image was successfully updated." }
+        format.json { render :show, status: :ok, location: @user_image }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user_image.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /user_images/1 or /user_images/1.json
+  def destroy
+    @user_image.destroy
+    respond_to do |format|
+      format.html { redirect_to user_images_url, notice: "User image was successfully destroyed." }
+      format.json { head :no_content }
     end
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
     def set_user_image
-      @user_image = UserImage.find_by(uuid: params[:id])
+      @user_image = UserImage.find(params[:id])
     end
 
-    def user_images
-      @user_image = UserImage.find_by(uuid: params[:id])
-    end
-
+    # Only allow a list of trusted parameters through.
     def user_image_params
-      params.require(:user_image).permit(:name, :pic)
-    end
-
-    def user_image(image)
-      image.attributes.merge(image: image.pic.attached? ?  url_for(image.pic) : '')
+      params.require(:user_image).permit(:name)
     end
 end
